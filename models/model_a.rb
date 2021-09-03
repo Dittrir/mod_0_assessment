a78308b38bf57fb418a13d5a8aed8018803c0ec8
Robin Dittrich <robinodittrich@gmail.com>
2021-09-03 16:03:57 -0700

    Create 'dinosaur' hash

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+# Create dinosaur hash and add key variables
+dinosaur = {
+  'name' => 'Triceratops',
+  'habitat' => 'Land',
+  'avg_weight_in_lbs' => 14000,
+  'avg_lenght_in_ft' => 28,
+  'carnivore' => false,
+  'extinct' => true,
+  'distinguishing_features' => ["three horns", "armor plate on head", "looks like rhino"],
+  'food_eaten' => ["grass", "shrubs", "trees"]
+}
+p dinosaur

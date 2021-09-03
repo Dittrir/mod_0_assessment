610632196c3d9171b089cd10294b854a5176e026
Robin Dittrich <robinodittrich@gmail.com>
2021-09-03 16:14:26 -0700

    Create 'developer' hash

---
+++ models/model_b.rb
@@ -0,0 +1,13 @@
+# Create hash model and assign variables
+developer = {
+  'name_first' => 'Robin',
+  'name_last' => 'Dittrich',
+  'age_in_years' => 27,
+  'time_coding_in_months' => 3,
+  'proficient_in_coding' => false,
+  'excited_about_coding' => true,
+  'exp_with_code_lang' => ["python", "ruby", "javascript"],
+  'hobbies' => ["climbing", "skiing", "paragliding"]
+}
+
+p developer

0deac9d0c8196a1871ad224d7113071be42b9a2c
Robin Dittrich <robinodittrich@gmail.com>
2021-09-03 15:47:11 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,19 @@
+# Assign variables
+movie = "Cliffhanger"
+num_4 = 4
+num_6 = 8
+
+# New variable derived from num_6 x num_4
+product = num_6 * num_4
+p product
+
+# Create array called 'favorite_foods'
+favorite_foods = ["Coconut", "Burritos", "Pasta"]
+p favorite_foods
+
+# Remove first variable
+favorite_foods.shift()
+p favorite_foods
+
+# Print number of characters in 'movie' string
+puts movie.length
